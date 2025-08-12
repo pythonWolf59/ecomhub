53f506cb61749feff70f350d6b39c46f27cc8f38
Hamza Sareer <humxazakir11@gmail.com>
2025-08-12 17:21:16 +0500

    Added CORS

--- ecomhub-ui/fast_cors.py
+++ ecomhub-ui/fast_cors.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://dkdigitalhub.netlify.app/"],
+    allow_origins=["http://localhost:3000", "https://dkdigitalhub.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
